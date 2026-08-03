6e5793daedebeb13e3febe2200d74370c399fdb9
himym413 <igorseslak7@gmail.com>
2026-08-03 19:59:07 +0200

    Add view rendering helper

--- app/helpers.php
+++ app/helpers.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 function view(string $path, array $data = []): void 
 {
-  extract($data);
-  
   $view = __DIR__ . '/../resources/views/' . $path . '.view.php';
-
+  
   if (!file_exists($view)) {
     throw new RuntimeException("View [{$path}] could not be found.");
-  }
+    }
+
+  extract($data);
 
   require $view;
 }
