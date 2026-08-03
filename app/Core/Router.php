3e5335b09f9813e411f66c59a2e4bef2a6b1a818
himym413 <igorseslak7@gmail.com>
2026-08-03 15:21:59 +0200

    Implement basic router

---
+++ app/Core/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+class Router 
+{
+  private array $routes = [];
+
+  private function add(string $method, string $uri, array $handler): void 
+  {
+    $this->routes[$method][$uri] = $handler;
+  }
+
+  public function dispatch(string $httpMethod, string $uri): void 
+  {
+    $handler = $this->routes[$httpMethod][$uri] ?? null;
+
+    if ($handler === null) {
+      http_response_code(404);
+      echo '404 Not Found';
+      return;
+    }
+
+    [$controllerClass, $controllerMethod] = $handler;
+
+    $controller = new $controllerClass();
+    $controller->$controllerMethod();
+  }
+
+  public function get(string $uri, array $handler): void 
+  {
+    $this->add('GET', $uri, $handler);
+  }
+
+  public function post(string $uri, array $handler): void 
+  {
+    $this->add('POST', $uri, $handler);
+  }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$router = require __DIR__ . '/../routes/web.php';
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '/';
+
+$router->dispatch($httpMethod, $uri);
+$router->dispatch('GET', '/transactions');
