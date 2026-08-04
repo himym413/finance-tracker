5d9047bff14673ea11c247f4d340fdc7ce4906a7
himym413 <igorseslak7@gmail.com>
2026-08-04 12:52:56 +0200

    Implement dependency injection container

---
+++ app/Core/Container.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use ReflectionClass;
+
+class Container
+{
+  private array $bindings = [];
+
+  public function bind(string $key, object $value): void
+  {
+    $this->bindings[$key] = $value;
+  }
+
+  public function resolve(string $class): object
+  {
+    if (isset($this->bindings[$class])) {
+      return $this->bindings[$class];
+    }
+
+    $reflection = new ReflectionClass($class);
+    $constructor = $reflection->getConstructor();
+
+    if ($constructor === null) {
+      return new $class();
+    }
+
+    $parameters = $constructor->getParameters();
+    $dependencies = [];
+
+    foreach ($parameters as $parameter) {
+      $type = $parameter->getType();
+
+      if ($type === null) {
+        throw new \RuntimeException(
+          "Cannot resolve parameter [{$parameter->getName()}] in class [{$class}]."
+        );
+      }
+
+      $dependencyClass = $type->getName();
+
+      $dependencies[] = $this->resolve($dependencyClass);
+    }
+
+    return $reflection->newInstanceArgs($dependencies);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Controllers\DashboardController;
 use App\Core\Router;
 
-$router = new Router();
+$router = new Router($container);
 
 $router->get('/', [DashboardController::class, 'index']);
 
